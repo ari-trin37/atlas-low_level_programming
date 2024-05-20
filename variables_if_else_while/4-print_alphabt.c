37e713548da4f946d6e026a584c173be73dcb03c
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-20 23:47:15 +0000

    q and e suck

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet except q and e for some reason
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if letter != 'q' && letter != 'e'
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
