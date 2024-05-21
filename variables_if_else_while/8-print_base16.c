4799656ebd6d3e84d47ffe86f9eeb1623e88380a
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-21 02:19:10 +0000

    three putchar

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,16 +9,14 @@
 int main(void)
 {
 	int num;
+	char letter;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar('0' + num);
-		if (num != 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
 	putchar('\n');
 
 	return (0);
