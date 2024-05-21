e1595719c96d934d905cf768b91d65e80e9fc298
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-21 02:16:49 +0000

    hex is hard

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,4 +8,18 @@
 
 int main(void)
 {
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
