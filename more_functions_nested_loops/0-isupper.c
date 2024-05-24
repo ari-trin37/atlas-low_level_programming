08b57af3297f4b778b5b1491cd8086f7cc92dab2
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-24 23:29:58 +0000

    commit second

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - this is the function name.
+ * @c: checked if uppercase.
+ *
+ * Return: returned zero for uppercase character.
+ */
+
+
+
+
+int _isupper(int c)
+{
+	if (65 <= c && c <= 90)
+		return (1);
+	else 
+		return (0);
+}
