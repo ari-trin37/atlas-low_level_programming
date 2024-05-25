c5a93d46942756671b3f04bbd6d761007c1f940a
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-25 20:20:43 +0000

    dine n dash

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two integars
+ * @a; this is first para.
+ * @b; this is second para.
+ *
+ * Return: using two integars
+ */
+
+int mul(int a, int b)
+{
+	return (a x b); 
+}
