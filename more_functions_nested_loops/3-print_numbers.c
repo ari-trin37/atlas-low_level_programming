21f983ec3f433c3d2fd35ac354bfc19a7a8c54ab
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-25 21:57:54 +0000

    high

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - printed zero through nine
+ *
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int x;
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}	
