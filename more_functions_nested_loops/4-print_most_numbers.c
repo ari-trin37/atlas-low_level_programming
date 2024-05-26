10acf0777ce70ab976e626e5f49fe4a70ae95fe7
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-26 17:43:54 +0000

    hola

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * print_most_numbers - printing some numbers.
- * @void: it is indeed a void.
+ * print_most_numbers - printing some numbers
+ * @void: it is indeed a void
  *
- * Return: returning all numbers but two and four.
+ * Return: returning all numbers but two and four
  */
 
 void print_most_numbers(void)
-{	
+{
 	char i;
-	
+
 	for (i = '0'; i <= '9'; ++i)
 	{
-		if(i != '2' && i != '4')	
+		if (i != '2' && i != '4')	
 			_putchar(i);
 	}
 
