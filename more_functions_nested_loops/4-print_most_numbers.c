a74a0efb9b0ad885067708b4c3f9791ce8960d14
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-26 15:42:04 +0000

    c 2

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_most_numbers - printing some numbers.
- * void: it is indeed a void.
+ * @void: it is indeed a void.
  *
  * Return: returning all numbers but two and four.
  */
