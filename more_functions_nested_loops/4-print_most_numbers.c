d80ba45d0ecc16e04a800bc6151ca39d9260caa7
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-26 17:24:58 +0000

    message

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,17 @@
  * Return: returning all numbers but two and four.
  */
 
-void print_most_numbers(void) {
+void print_most_numbers(void)
+{	
 	char i;
+	
 	for (i = '0'; i <= '9'; ++i)
-		if(i != '2' && i != '4')
-			_putchar (i);
+	{
+		if(i != '2' && i != '4')	
+			_putchar(i)
+	}
+
+	_putchar ("\n")
 }
 		
 	
