0470384416859ae32cb0ca60e6cd13563fbec55e
ari-trin37 <arielle.taylor@atlasschool.com>
2024-05-27 19:35:33 +0000

    message

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * print_diagonal - printing diagonal line.
+ * @n: new line
+ *
+ * Return: printed line
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int x;
+	if(n > 0)
+	{
+		for(i = 0; i < n; i++)
+	}
+
+	{
+		for(x = 0; x > i; x++)
+	}
+
+	{
+		_putchar(' ');
+	}
+
+	{
+		_putchar('\\');
+	}
+
+	{	_putchar('\n');
+
+	}
+
+}
+
+		else
+{
+	_putchar('\n');
+}
