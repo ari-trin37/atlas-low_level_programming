79e52946fa062431ca5f4b7814e2c43216b915f8
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-05-27 15:34:31 -0500

    used code from 7

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,27 @@
 #include "main.h"
 /**
- * print_diagonal - printing diagonal line.
- * @n: new line
+ * print_square - printing square.
+ * @size: new line
  *
- * Return: printed line
+ * Return: creating one squares.
  */
+void print_square(int size)
+{
+int i;
+int x;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (x = 0; x < size; x++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
