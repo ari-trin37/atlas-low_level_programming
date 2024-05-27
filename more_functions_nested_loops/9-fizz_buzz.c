9571b38868d94f82f7453906bd7396867dbba7d8
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-05-27 17:17:05 -0500

    you tube is graet

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* fizz_buzz - fizzin and buzzin
+*/
+int main(void)
+{
+    int x;
+    for (x = 1; x <= 100; x++)
+    {
+        if (x % 5 == 0)
+        {
+        printf("Buzz");
+        }
+        else if (x % 3 == 0)
+        {
+        printf("Fizz");
+        }
+        else if(x % 15 == 0)
+        {
+        printf("FizzBuzz");
+        }
+        else if(x == 1)
+        {
+        printf("%d", x);
+        }
+        else
+        {
+        printf("%d", x);
+        }
+    }
+{
+    _putchar('\n');
+    return (0);
+}
