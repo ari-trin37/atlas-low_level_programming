aafe04f37c5c95a7ef4adf5494ac0a8a9475e22f
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-05-27 15:11:30 -0500

    Update 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,11 +10,11 @@ void print_diagonal(int n)
 {
 int i;
 int x;
-if(n > 0)
+if (n > 0)
 {
-for(i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
-for(x = 0; x < i; x++)
+for (x = 0; x < i; x++)
 {
 _putchar(' ');
 }
