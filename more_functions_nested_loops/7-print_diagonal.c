89050c774b741925ae2956f08940ff9457384b12
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-05-27 15:03:34 -0500

    Update 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,31 +13,22 @@ int x;
 if(n > 0)
 {
 for(i = 0; i < n; i++)
-}
-
 {
 for(x = 0; x > i; x++)
-}
-
 {
 _putchar(' ');
 }
 
 {
 _putchar('\\');
-}
-
-{	
 _putchar('\n');
-
 }
 
 }
 
-{
+
 else
-}
-
 {
 _putchar('\n');
+}
 }
