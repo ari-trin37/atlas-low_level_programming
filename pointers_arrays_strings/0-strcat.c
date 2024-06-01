c020f21e18b152fa877a7e37c5f5544053d60e3d
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-01 23:06:19 +0000

    world

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,15 +12,15 @@ char *_strcat(char *dest, char *src)
 	int x = 0;
 	int i = 0;
 
-	while(dest[x] != '\0')
+	while (dest[x] != '\0')
 	{
 		x++;
 	}
-	while(src[i] != '\0')
+	while (src[i] != '\0')
 	{
 		dest[i + x] = src[i];
 		i++;
 	}
 	dest[i + x] = '\0';
-	return(dest);
+	return (dest);
 }
