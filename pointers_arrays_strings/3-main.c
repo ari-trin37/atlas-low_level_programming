c480b90a4846efceec662d618d2680397bc53121
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-02 22:59:53 +0000

    idk

---
+++ pointers_arrays_strings/3-main.c
@@ -0,0 +1,25 @@
+en@ubuntu:~/$ cat 3-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    char s1[] = "Hello";
+		    char s2[] = "World!";
+
+			    printf("%d\n", _strcmp(s1, s2));
+				    printf("%d\n", _strcmp(s2, s1));
+					    printf("%d\n", _strcmp(s1, s1));
+						    return (0);
+}
+julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strcmp.c -o 3-strcmp
+julien@ubuntu:~/$ ./3-strcmp
+-15
+15
+0
+julien@ubuntu:~/$
