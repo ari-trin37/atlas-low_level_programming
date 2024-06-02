a35ef46655e58daf6af985d10594a80115ae94e4
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-02 19:23:33 +0000

    attitude

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1 : para one
+ * @s2 : para two
+ *
+ * Return : can be less than, greater than, or equal to.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int y = 0;
+
+	for (y = 0; s1[y] != '\0' && s2[y] != '\0'; y++)
+		if (s1[x] != s2[x]) {
+			result = s1[y] - s2[y];
+			break;
+		}
+		if (s1[y] == '\0' && s2[y] != '\0') {
+			result = -1;
+		} else if (s1[y] != '\0' && s2[y] == '\0') {
+			result = 1;
+		}
+
+		return result;
+}
+
+
