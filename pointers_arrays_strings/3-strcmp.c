f21efdc3466497249afbe6a25245ceadb4a6b45e
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-03 20:59:03 +0000

    hi

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int y = 0;
-	for (;s1[y] != '\0' && s2[y] != '\0'; y++)
+	while (s1 && (s1 == s2))
 	{
-	if (s1[y] != s2[y])
-      	{
-
-			return (s1[y] - s1[1]); }}
-	 		return(0); 
+		s1++;
+		s2++;
+	}
+	return *(unsigned char *) s1 - *(unsigned char *)s2;
 }
