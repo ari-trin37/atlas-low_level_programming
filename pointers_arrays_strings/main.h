07d6ca831ad44ce231e212ea4942d4bc411a28af
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 18:04:44 -0500

    added two prototypes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,3 +12,5 @@ void rev_string(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+char *string_toupper(char *);
+char *cap_string(char *);
