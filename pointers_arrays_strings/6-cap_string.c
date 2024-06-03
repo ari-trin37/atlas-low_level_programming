b99f72a6a599fe22930d6881e4688d116aa73f64
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 17:57:48 -0500

    change int

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -7,9 +7,10 @@
  */
 
  char *cap_string(char *) {
-    int capitalize_next = 1; 
+    int capitalize_next = 1;
+    int  i;
 
- for (int i = 0; str[i] != '\0'; i++) {
+ for (i = 0; str[i] != '\0'; i++) {
         if (capitalize_next && islower((unsigned char)str[i])) {
             str[i] = toupper((unsigned char)str[i]);
             capitalize_next = 0; 
