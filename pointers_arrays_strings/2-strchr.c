c7894bd90bdad6486fcbc0846230578c32444efa
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 22:25:38 -0500

    fix indents

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,15 +3,15 @@
  * _memset - fills first n bytes of memory area pointed to s.
  * @s: para one
  * @c: para two
- * 
+ *
  * Return: returns pointer to memory area s.
  */
 
- char *_strchr(char *s, char c)
- 
+char *_strchr(char *s, char c)
+
 {
-    { 
-    for (;; s++)
+    {
+for (;; s++)
     {
     if (*s == c)
     return (s);
