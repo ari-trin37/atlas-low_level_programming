d758cfd0f74ca7aa8002bc5a428981f0545081b9
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 23:00:07 -0500

    fix betty

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 /**
  * _memset - fills first n bytes of memory area pointed to s.
  * @a: para one
- * 
+ *
  * Return: returns pointer to memory area s.
  */
 
@@ -11,8 +11,10 @@ void print_chessboard(char (*a)[8])
 {
 int i, j;
 
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
+    for (i = 0; i < 8; i++) 
+    {
+        for (j = 0; j < 8; j++) 
+        {
             printf("%c", a[i][j]);
         }
         printf("\n");
