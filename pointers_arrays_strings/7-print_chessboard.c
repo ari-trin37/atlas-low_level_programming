3e6fe11e4a5060e9770902c4bc3ea463f622c74b
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 22:57:03 -0500

    test

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -6,5 +6,15 @@
  * Return: returns pointer to memory area s.
  */
 
- void print_chessboard(char (*a)[8])
- 
+void print_chessboard(char (*a)[8]
+
+{
+int i, j;
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            printf("%c ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
