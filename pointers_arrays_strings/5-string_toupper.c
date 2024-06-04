7aa587c9cec0052891c440ff5abf53cf1dd0dd07
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 19:15:41 -0500

    wegotit

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,12 @@ char *string_toupper(char *s)
 int i;
 i = 0;
 
-while (s[i] != '\0') {
-	if (s[i] >= 'a' && s[i] <= 'z') 
+while (s[i] != '\0')
+{
+	if (s[i] >= 'a' && s[i] <= 'z')
 	s[i] -= 32;
 	i++;
 
-	}
+}
 return (s);
 }
