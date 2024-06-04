610ac03395b3c5c568d0bffe9269289c75522baf
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 22:41:30 -0500

    fixing doc

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,10 @@
 unsigned int _strspn(char *s, char *accept)
 
 {
-    unsigned int count = 0;
-    int found = 0;
-    while (*s != '\0') {
+unsigned int count = 0;
+int found = 0;
+
+while (*s != '\0') {
         char *a = accept;
         found = 0;
         while (*a != '\0') {
