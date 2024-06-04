279a8f03b16886cb68398ee8b879d7391193afec
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 22:33:45 -0500

    seems to work

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,3 +9,26 @@
  */
 
 unsigned int _strspn(char *s, char *accept)
+
+{
+    unsigned int count = 0;
+    int found = 0;
+    while (*s != '\0') {
+        char *a = accept;
+        found = 0;
+        while (*a != '\0') {
+            if (*s == *a) {
+                found = 1;
+                break;
+            }
+            a++;
+        }
+        if (found) {
+            count++;
+        } else {
+            break;
+        }
+        s++;
+    }
+    return count;
+}
