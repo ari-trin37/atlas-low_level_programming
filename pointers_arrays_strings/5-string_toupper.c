b95f03317ffc9496cc94455cab9a6c0ff7e94353
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 19:14:33 -0500

    changing trillion times

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,14 @@
 
 char *string_toupper(char *s)
 {
-	int i;
-	i = 0;
+int i;
+i = 0;
 
-    while (s[i] != '\0') {
+while (s[i] != '\0') {
 	if (s[i] >= 'a' && s[i] <= 'z') 
 	s[i] -= 32;
 	i++;
 
 	}
-    return (s);
+return (s);
 }
