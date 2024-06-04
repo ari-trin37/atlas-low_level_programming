be64d856c995a6b7d7904a7ef8fec15946026399
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-03 23:17:52 -0500

    fix white space

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -4,33 +4,33 @@
  * @s: para one
  * @b: para two
  * @n: para three
- * 
+ *
  * Return: returns pointer to memory area s.
  */
 
 char *_strstr(char *haystack, char *needle)
 
 {
-    if (*needle == '\0') 
-    {
-        return haystack;
-    }
-    while (*haystack != '\0')
-    {
-        char *h = haystack;
-        char *n = needle;
+  if (*needle == '\0')
+  {
+   return haystack;
+  }
+   while (*haystack != '\0')
+  {
+   	char *h = haystack;
+    char *n = needle;
 
-        while (*h != '\0' && *n != '\0' && *h == *n) {
-            h++;
-            n++;
-        }
-        if (*n == '\0') 
-        {
-            return haystack;
-        }
+    while (*h != '\0' && *n != '\0' && *h == *n) {
+    h++;
+    n++;
+    	}
+     if (*n == '\0') 
+     {
+     return haystack;
+     }
 
-        haystack++;
-    }
+     haystack++;
+  }
 
-    return (NULL);
+  return (NULL);
 }
