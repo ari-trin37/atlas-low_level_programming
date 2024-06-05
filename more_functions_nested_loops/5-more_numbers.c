f4efdb05eefce4b4dcaaddf1425ad5755c6dd09f
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-05 18:13:32 -0500

    attempt 1

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,21 +8,20 @@
 
 void more_numbers(void)
 {
-	int y;
-	int x;
+	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x < 10; x++) 
 	{
-		for (y = 0; y < 15; y++)
+		for (y = 0; y < 15; y++)  
 		{
-		
 			if (y >= 10)
-		       	{
-				_putchar('0' + (y / 10));
-	        }
-		    _putchar('0' + (y % 10));
-	{
-	       	_putchar('\n');
+				_putchar('0' + (y / 10)); 
+			
+		    _putchar('0' + (y % 10)); 
+		}
+	 	_putchar('\n');
 	}
 }
+
+
 
