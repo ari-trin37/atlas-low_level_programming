0061cf90fba540fc86a64325b2fa2eb0964c612d
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-06 19:36:30 +0000

    saving time

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * jack_bauer - function name
+ * @void : zero return, void.
+ *
+ * Return: zero 
+ */
+
+void jack_bauer(void)
+{ 
+	for (
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,4 @@
+##include "main.h"
+/**
+ * times_table - function name
+ * void - 
