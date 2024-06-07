3c94c0630170fc4fc6b271a3ddfea69e2c14fa52
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-06 22:37:00 -0500

    fix logic

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,29 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* fizz_buzz - fizzin and buzzin
-*/
+ * fizz_buzz - fizzing and buzzing
+ */
 int main(void)
 {
     int x;
     for (x = 1; x <= 100; x++)
     {
-        if (x % 5 == 0)
+        if (x % 15 == 0)
         {
-        printf("Buzz");
+            printf("FizzBuzz");
         }
         else if (x % 3 == 0)
         {
-        printf("Fizz");
+            printf("Fizz");
         }
-        else if(x % 15 == 0)
+        else if (x % 5 == 0)
         {
-        printf("FizzBuzz");
-        }
-        else if(x == 1)
-        {
-        printf("%d", x);
+            printf("Buzz");
         }
         else
         {
-        printf("%d", x);
+            printf("%d", x);
         }
     }
 
