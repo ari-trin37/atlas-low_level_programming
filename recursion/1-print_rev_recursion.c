ad5696d009656fc2aba0b64e2f2fd2cf64cb88b3
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-07 13:36:04 -0500

    attempt1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _print_rev_recursion - string in reverse.
+* @s: our variable.
+* return: returning a string in reverse.
+*
+*/
+
+void _print_rev_recursion(char *s)
+{ 
+    if (*s == '\0')
+    {
+        return;
+    }
+
+    
+    _print_rev_recursion(s + 1);
+    return (*s);
+}
