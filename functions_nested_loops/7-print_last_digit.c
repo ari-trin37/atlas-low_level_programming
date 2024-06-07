e7c64d69b4ea7c3ec5a12b904f6042b19f4dfa6b
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-06 22:27:43 -0500

    removed indent to fix betty

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -5,21 +5,20 @@
  *
  * Return : returns value of last digit
  */
-
-	int print_last_digit(int num)
+int print_last_digit(int num)
 
 {
-	num = (num % 10);
-	if (num >= 0)
-		{
-			_putchar('0' + num);
-			return (num);
-		}
-	else if (num < 0)
-		{
-		num = (num * -1);
-		_putchar('0' + num);
-		return (num);
-		}
-	return (0);
+num = (num % 10);
+if (num >= 0)
+{
+_putchar('0' + num);
+return (num);
+}
+else if (num < 0)
+{
+num = (num * -1);
+_putchar('0' + num);
+return (num);
+}
+return (0);
 }
