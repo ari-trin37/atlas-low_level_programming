554d8af8d44a324ccf628f76d6f431f569b3b7c6
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-07 13:00:26 -0500

    commit 3

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -10,9 +10,9 @@ void _puts_recursion(char *s)
     if (*s == '\0')
     {
         _putchar('\n');
-        return 0;
+        return;
     }
 
-        return 0;
         _putchar('\n');
+        return;
 }
