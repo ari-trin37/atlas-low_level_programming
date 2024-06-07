06f67ef949c9b16690e48ac1104d374ef0e3c6e5
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-06 22:41:34 -0500

    fix main

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,43 @@
 /**
  * fizz_buzz - fizzing and buzzing
  */
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * fizz_buzz - fizzing and buzzing
+ */
 void fizz_buzz(void)
+{
+    int x;
+    for (x = 1; x <= 100; x++)
+    {
+        if (x % 15 == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else if (x % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        else if (x % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+            printf("%d", x);
+        }
+    }
+
+    putchar('\n');
+}
+
+int main(void)
+{
+    fizz_buzz();
+    return 0;
+}
 {
     int x;
     for (x = 1; x <= 100; x++)
