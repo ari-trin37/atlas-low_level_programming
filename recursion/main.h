eee460f1e6d0328275c217e9458c6e1eac9e9d3b
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-07 13:15:16 -0500

    added important lines

--- recursion/main.h
+++ recursion/main.h
@@ -1,3 +1,10 @@
+#ifndef main_h
+#define main_h
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -5,4 +12,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _putchar(char c)
+int _putchar(char c);
+
+
