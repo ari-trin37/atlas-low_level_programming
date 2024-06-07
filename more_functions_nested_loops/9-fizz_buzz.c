c56753af6a291782e74fb293ce7ed15353e4daa9
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-06 22:44:00 -0500

    fix space

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ void fizz_buzz(void)
     {
         if (x % 15 == 0)
         {
-            printf("FizzBuzz");
+            printf("FizzBuzz ");
         }
         else if (x % 3 == 0)
         {
-            printf("Fizz");
+            printf("Fizz ");
         }
         else if (x % 5 == 0)
         {
-            printf("Buzz");
+            printf("Buzz ");
         }
         else
         {
-            printf("%d", x);
+            printf("%d ", x);
         }
     }
 
