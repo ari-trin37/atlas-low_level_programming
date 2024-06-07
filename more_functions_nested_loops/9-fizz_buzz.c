b48c850cfb2a83a94efd4db2a5fc7bac3ec0b032
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-06 22:51:56 -0500

    removed indents to fix

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,36 +6,34 @@
  */
 void fizz_buzz(void)
 {
-    int x;
-    for (x = 1; x <= 100; x++)
-    {
-        if (x % 15 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (x % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (x % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", x);
-        }
-        if (x < 100)
-        {
-            printf(" ");
-        }
-    }
-
-    putchar('\n');
+int x;
+for (x = 1; x <= 100; x++)
+{
+if (x % 15 == 0)
+{
+printf("FizzBuzz")
 }
-
+else if (x % 3 == 0)
+{
+printf("Fizz");
+}
+else if (x % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", x);
+}
+if (x < 100)
+{
+printf(" ");
+}
+}
+putchar('\n');
+}
 int main(void)
 {
-    fizz_buzz();
-    return 0;
+fizz_buzz();
+return 0;
 }
