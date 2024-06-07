c6603f52a202f38b54d6bbcf73a7af33e3a45958
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-07 01:54:48 +0000

    hi

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _isalpha - function 
+* _isalpha - function
 * @c : variable
 *
 * Return : returns one, c is a letter, lower or uppercase.
@@ -8,5 +8,5 @@
 
 int _isalpha(int c)
 {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
 }
