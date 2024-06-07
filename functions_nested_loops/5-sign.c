ec4f928ae52fdf07a0be89760240d05ca7c38516
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-06 22:03:53 -0500

    bettys fault

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  * print_sign - function
  * @n: variable
  *
- * Return : returns one, c is a letter, lower or uppercase.
+ * Return: returns one, c is a letter, lower or uppercase.
  */
 
 int print_sign(int n)
