1f5b60e2e3feb4371e4f71144de6a0a870298fda
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-08 16:59:42 -0500

    fixed multiple errors

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -12,11 +12,13 @@ void _print_rev_recursion(char *s)
     {
         ;
     }
-        else
+    else
     {
+
         _print_rev_recursion(s + 1);
+        _putchar(*s)
     }
     if (*s != '/0')  
-        _putchar(*s);
+ _putchar(*s);
 }
         
