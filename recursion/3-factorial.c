9353d80033f2bceba80d6a1040b3f353a8efb41c
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-09 14:52:48 -0500

    changed indentation, deleted lines

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,17 +8,12 @@
 
 int factorial(int n)
 {
-	if (n < 0)
-	{	return (-1);
+	if (n < 0) {
+		return (-1); }
 
-	}
-		else if (n == 0)
 	{
+		else if (n == 0)
 		return (1);
-	}
-	
-	{	else
-
 	}
 		return n * factorial(n - 1);
 }
