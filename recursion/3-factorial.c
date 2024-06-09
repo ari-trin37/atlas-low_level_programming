f7499544930d966efdbbecf0bf85113d9e0eaf2c
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-09 15:08:59 -0500

    moving things around, attempting new code

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,11 +9,14 @@
 int factorial(int n)
 {
 	if (n < 0) {
+	{
 		return -1; 
 	}
 
-	if (n == 0) {
+	if (n == 0) 
+	{
 		return 1;
-	} else if {
-		return n * factorial(n - 1); }
+	} 	else if
+
+	{	return n * factorial(n - 1); }
 }
