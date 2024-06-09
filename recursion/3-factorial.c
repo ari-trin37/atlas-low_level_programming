17691c3f4baf2b2539b5c0af83af317640330066
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-09 15:31:43 -0500

    trying to fix code

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -10,13 +10,9 @@ int factorial(int n)
 {
 	if (n < 0) {
 		return -1; 
-	} 
-	
-	else if (n == 0) {
-		return 1; 
-	}
- 
-	else {
+	} else if (n == 0 || n == 1) {
+		return 1;
+	} else {
 		return (n * factorial(n - 1)); }
 		return (0);
 }
