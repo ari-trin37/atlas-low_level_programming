e9cc5d191e51da926927aea324a05c76cd93eeb3
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-09 18:39:57 -0500

    commit1

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,25 @@
 #include "main.h"
 /**
-* _strlen_recursion - fucntion name / returning length of string.
-* @s: variable given, includes a pointer. string - calculating length.
-*  
-* return : returning the length of a string
+* _pow_recursion - fucntion name / returning length of string.
+* @x: variable 1. includes a pointer. string - calculating length.
+* @y: varibale 2.
+*
+* return : returns value of x raised to the power of 5.
 */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (x *_pow_recursion(x, y - 1));
+	}
+	
+}
