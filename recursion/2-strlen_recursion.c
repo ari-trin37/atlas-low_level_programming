0d42da0ec2ae46a752ba668227a04f6120b5215c
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-08 19:33:00 -0500

    added #endif to end of file

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,12 +3,12 @@
 * _strlen_recursion - fucntion name / returning length of string.
 * @s: variable given, includes a pointer. string - calculating length.
 *  
-* return: returning the length of a string
+* return : returning the length of a string
 */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '/0') 
+	if (*s == '\0') 
 		return 0;
 	else 
 		return 1 + _strlen_recursion(s + 1);
