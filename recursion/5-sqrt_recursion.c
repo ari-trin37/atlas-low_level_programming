f51425ec2082ba80b31b7b700898e97ce4c295a9
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 13:53:30 -0500

    first commit

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,7 +5,20 @@
 *  
 * return : returning the length of a string
 */
+
 int _sqrt_recursion(int n)
 { 
-	
+	int val = 1;
+	return (_lumi_light (val, n));
+
+int _lumi_light(int val, int n)
+	{
+		if (n == val * val)
+			return (val);
+
+		else if (val > n)
+			return (-1);
+			return _lumi_light(val + 1, n)
+	}
 }
+
