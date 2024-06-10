ffb52e14f30fd6de0826ac3c5c1d48d4e0803032
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 17:06:15 -0500

    test2

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
-* _sqrt_recursion - function name / returning length of string.
 * _lumi_light - new function created.
-* @n: variable given, includes a pointer. string - calculating length.
 * @val: created new variable for 2nd prototype.
 *
 * Return: returning the length of a string
@@ -22,7 +20,7 @@ int _lumi_light(int val, int n)
 * _sqrt_recursion - function name given.
 * @n: our variable / where the sq. root will be calculated
 *
-* Return: either n or -1
+* Return: either n or -1.
 */
 
 int _sqrt_recursion(int n)
