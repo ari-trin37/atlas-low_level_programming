c9efb31558606c7082ebe76948d8789f55979234
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 14:56:10 -0500

    fixed indentation of line 24, switched prototype position, switched code

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,24 +1,25 @@
 #include "main.h"
 /**
-* _strlen_recursion - fucntion name / returning length of string.
+* _sqrt_recursion - function name / returning length of string.
+* _lumi_light - new function created.
 * @s: variable given, includes a pointer. string - calculating length.
-*  
-* return : returning the length of a string
+* @n: created new variable for 2nd prototype.
+*
+* Return: returning the length of a string
 */
 
-int _sqrt_recursion(int n)
-{ 
-	int val = 1;
-	return (_lumi_light (val, n));
-}
-
 int _lumi_light(int val, int n)
-	{
-		int _lumi_light; 
-		if (n == val * val)
-			return (val);
+{
+	if (n == val * val)
+		return (val);
+	else if (val > n)
+		return (-1);
 
-		else if (val > n)
-			return (-1);
-			return _lumi_light(val + 1, n);
-	}
+	return _lumi_light (val + 1, n);
+}
+int _sqrt_recursion(int n)
+{
+		int val = 1;
+
+		return (_lumi_light(val, n));
+}
