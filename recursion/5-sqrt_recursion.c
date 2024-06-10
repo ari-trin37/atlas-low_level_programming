abb6680a53e467a4b1d38a4bf7876b872ee9f631
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 17:02:29 -0500

    deleted trailing white space, #25

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -22,7 +22,7 @@ int _lumi_light(int val, int n)
 * _sqrt_recursion - function name given.
 * @n: our variable / where the sq. root will be calculated
 *
-* Return: either n or -1 
+* Return: either n or -1
 */
 
 int _sqrt_recursion(int n)
