6a3e5ac7ae42caeaaa6416cee9459a28d9a30d6e
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-09 19:37:47 -0500

    attempt2

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -5,7 +5,42 @@
 * @y: varibale 2.
 *
 * return : returns value of x raised to the power of 5.
-*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{ return (1);
+	}
+	if (y < 0)
+	{ return (-1)
+	}
+	return (x * _pow_recursion(x, y - 1));
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
