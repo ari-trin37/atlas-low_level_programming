63dafc688c00f9b5c52f5180ff9a8fbdcc417087
ari-trin37 <168394495+ari-trin37@users.noreply.github.com>
2024-06-11 15:09:32 -0500

    Update main.c, included header/functions

--- static_libraries/main.c
+++ static_libraries/main.c
@@ -1,30 +1,31 @@
 #include "main.h"
 
-0-memset.c
-0-reset_to_98.c
-0-strcat.c
-1-memcpy.c
-1-strncat.c
-1-swap.c
-100-atoi.c
-2-strchr.c
-2-strlen.c
-2-strncpy.c
-3-main.c
-3-puts.c
-3-strcmp.c
-3-strspn.c
-4-print_rev.c
-4-rev_array.c
-4-strpbrk.c
-5-rev_string.c
-5-string_toupper.c
-5-strstr.c
-6-cap_string.c
-6-puts2.c
-7-leet.c
-7-print_chessboard.c
-7-puts_half.c
-8-print_array.c
-8-print_diagsums.c
-9-strcpy.c 
+#ifndef main_h
+#define main_h
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _putchar(char c);
+
+#endif
