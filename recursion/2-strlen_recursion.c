af2ffdecd290419fe59d7e113c50be26cad5fbd5
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 20:29:19 -0500

    attempted betty fix

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,16 +2,16 @@
 /**
 * _strlen_recursion - fucntion name / returning length of string.
 * @s: variable given, includes a pointer. string - calculating length.
-*  
+*
 * return : returning the length of a string
 */
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0') 
-		return 0;
-	else 
-		return 1 + _strlen_recursion(s + 1);
+	if (*s == '\0')
+		return(0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 
 }
 
