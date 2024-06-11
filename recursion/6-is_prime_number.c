02a48c082e6f4e5a5ef3631281b4f8eeb1133d33
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 20:03:54 -0500

    attempting to rearrange code/indentation/etc.

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,22 +10,21 @@
 * ex: 2,3,5,7,11,etc.
 **/
 int is_prime_number(int n)
-{
-	if (n <= 1);
-	{
-		return(0);
-	}
 
-	for (i = 2; i * i <= n; i++)
+{
+	for (i = 1; i * i <= n; i++)
+	{ 
+		if (n % i == 0)
+			return(0);
+	}
 	{
-		if (n % i == 0);
+		if (n <= 1);
 		{
 			return(0);
 		}
 
+		return(0);
 	}
 
-	return(1);
 }
-
 
