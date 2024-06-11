acb4df11120514a5ad5ac4eb28834cb99a02cfad
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 20:33:37 -0500

    testing betty technique

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,8 +1,5 @@
 #include "main.h"
 /**
-* _strlen_recursion - fucntion name / returning length of string.
-* @s: variable given, includes a pointer. string - calculating length.
-*
 * return : returning the length of a string
 */
 
