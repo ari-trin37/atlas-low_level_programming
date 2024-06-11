71b7659d37bb8d646d6f54b8f8bc962f8ad3c24d
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 19:53:33 -0500

    code revised1

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -11,13 +11,21 @@
 **/
 int is_prime_number(int n)
 {
-	for (int i = 2; i * i <= n; i++)
-	{ 
+	if (n <= 1);
+	{
 		return(0);
 	}
-		if (n <= 1)
-	{	
-		return(0);
+
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0);
+		{
+			return(0);
+		}
+
 	}
-	return(0);
+
+	return(1);
 }
+
+
