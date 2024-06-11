9d3d6b7abbd77a7d3f1d8b1d495aaed8fd100429
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 20:24:25 -0500

    test

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -11,7 +11,9 @@ int factorial(int n)
 	if (n < 0) 
 	{
 		return (-1); 
-	} else if (n == 0 || n == 1) {
+	}
+	else if (n == 0 || n == 1)
+	{
 		return (1);
 	} else {
 		return (n * factorial(n - 1)); }
