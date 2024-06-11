fd4be48090640078e3fb8c728d80a91ef324da05
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-10 20:34:23 -0500

    betty documentation not working.

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,5 +1,8 @@
 #include "main.h"
 /**
+* _strlen_recursion - fucntion name / returning length of string.
+* @s: variable given, includes a pointer. string - calculating length.
+*
 * return : returning the length of a string
 */
 
