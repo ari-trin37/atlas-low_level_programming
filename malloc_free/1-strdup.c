0e06d451508c29ecd3efec16d2b55f9bb7d413e0
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-13 17:18:33 -0500

    attempted change to line 19, char *

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -16,12 +16,12 @@ char *_strdup(char *str)
 		return(NULL); 
 	}
 
-	while (str[length] != '\0') /* calculating length of string */
+	while (char *[length] != '\0') /* calculating length of string */
 	{
 		 length++;
 	}
 
-	char duplicate;
+	duplicate = duplicate
 
 	duplicate = malloc((length + 1) * sizeof(char)); 
 	/* allocate memory 4 duplicate string */
