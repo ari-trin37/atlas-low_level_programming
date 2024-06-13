db0c473e3f402df932d0a1eb84ab0a65691d388c
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-12 21:40:35 -0500

    change

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * str_concat - 
+ * str_concat - main function / working on better understanding concept.
  * @s1: 1st var. / will connect with string 2.
  * @s2: 2nd var. / in connection w/ s1.
  * 
@@ -9,3 +9,8 @@
  **/
 \** added onto **/
 char *str_concat(char *s1, char *s2)
+{
+	void
+	return (0);
+}
+	return(0);
