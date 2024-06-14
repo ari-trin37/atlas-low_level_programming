9032af28a5380ae1d6c5b27e9529ea612859f41d
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-14 17:42:42 -0500

    update

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,38 +9,33 @@
 
 int **alloc_grid(int width, int height)
 {
-	int x;
-	int z;
+    int **grid;
+    int i, x, z;
 
-	if (width <= 0 || height <= 0)
-		{
-			return(NULL);
-		}
+    if (width <= 0 || height <= 0)
+        return NULL;
 
-	int **grid;
-	grid = (int **)malloc(height * sizeof(int *));
-	if (!grid)
-		{
-			return(NULL); /* "check" to see if mem. allocation failed */	
-		}
+    grid = (int **)malloc(height * sizeof(int *));
+    if (grid == NULL)
+        return NULL; // Memory allocation failed
 
-	for (int i = 0; i < height; i++) /* makes new memory for rows */
-		grid[i] = malloc(width * sizeof(int));
-		if (!grid[i]) /* failure */
+    for (i = 0; i < height; i++)
+    {
+        grid[i] = (int *)malloc(width * sizeof(int));
+        if (grid[i] == NULL)
+        {
+            // Free previously allocated memory and return NULL on failure
+            for (x = 0; x < i; x++)
+                free(grid[x]);
+            free(grid);
+            return NULL;
+        }
 
-			for (int x = 0; x < i; x++) /* make new space for rows */
-			{
-				free(grid[x]);
-			}
-			free(grid); /* frees memory for the rows*/
-			{
-				return(NULL) /* failure for row */
-			}
+        for (z = 0; z < width; z++)
+        {
+            grid[i][z] = 0;
+        }
+    }
 
-		for (int z = 0; z < width; z++)
-		{
-			grid[i][z] = 0;
-		}
-
-		return(grid);
+    return grid;
 }
