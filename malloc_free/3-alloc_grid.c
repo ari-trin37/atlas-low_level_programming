2697bc10df3dd0cbf8900bcfaa49383c78435646
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-14 17:19:09 -0500

    commit1

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -6,10 +6,39 @@
  * Return: NULL due to w/h being 0.
  * 
  **/
-\** creating grid w/ double pointer **/
 
 int **alloc_grid(int width, int height);
 {
+	int x;
+	int z;
+	int i;
 
-} 
+	if (width <= 0 || height <= 0)
+		{
+			return(NULL;)
+		}
 
+
+	int **grid = (int **)malloc(height * sizeof(int *));
+	if (!grid)
+		{
+			return(NULL); /* "check" to see if mem. allocation failed */	
+		}
+
+	for (int i = 0; i < height; i++) /* makes new memory for rows */
+		grid[i] = malloc(width * sizeof(int));
+		if (!grid[i]) /* failure */
+
+			for (int x = 0; x < i; x++) /* make new space for rows */
+			{
+				free(grid[x]);
+			}
+			free(grid); /* frees memory for the rows*/
+			return(NULL) /* failure for row */
+
+		for (int z = 0; z < width; z++)
+		{
+			grid[i][z] = 0;
+		} 
+	}
+	
