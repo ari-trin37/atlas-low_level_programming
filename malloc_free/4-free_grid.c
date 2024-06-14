57ba5c971898b3e43489e81b79a2d307c4363338
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-14 18:39:42 -0500

    attempt

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -6,7 +6,18 @@
  * 
  * Return: nothing
  **/
-\** double pointer -an array of arrays. **/
 
 void free_grid(int **grid, int height)
+{
+	int x;
 
+	if (grid == NULL || height <= 0)
+		{
+			return;
+		}
+	for (x = 0; x < height; x++)
+	{
+		free(grid[x]);
+	}
+	free(grid);
+}
