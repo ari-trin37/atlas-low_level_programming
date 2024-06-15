f17a8825a9f239604918aaf99c2e6cb7bb0aeeb3
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-15 13:54:27 -0500

    first attempt

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,20 @@
 #include "main.h"
 /**
  * malloc_checked -creating function that allocates memory
- * @b: variable
- * 
+ * @b: represents size of memory block that'll be allocated.
+ *
  * Return: returns pointer to the allocated memory.
  */
 
 void *malloc_checked(unsigned int b)
 {
+	int b; /* declaration */
+	void *ptr = malloc(b); /* allocate memory */
+
+	if (!ptr) /* did allocation fail? */
+	{
+		exit(98);
+	}
 	
+	return ptr; /* returned pointer to allocated memory */
 }
