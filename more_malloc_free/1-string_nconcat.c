a83eb25d891085b723782151c5584e02415d7bfa
FQX117 <blackfox950@gmail.com>
2024-06-16 13:11:14 -0500

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
-/**
- **/char *string_nconcat(char *s1, char *s2, unsigned int n)
+/*
+ */char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1 = 0, len2 = 0, i, j;
 	char *concat;
