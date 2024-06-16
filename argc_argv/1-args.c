3d8bd5b16b9643dd2942f181e7b29af7b3066b13
FQX117 <blackfox950@gmail.com>
2024-06-16 15:01:54 -0500

    new idea

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -6,8 +6,13 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
-    for (i = 0; i < argc; i++)
-    printf("%s\n", argv[i]);
-    return 0;
+	(void)argc;
+	(void)argv;
+
+	int i;
+	for (i = 0; i < argc; i++) 
+	{
+		printf("%s\n", argv[i]);
+	}
+	return 0;
 }
