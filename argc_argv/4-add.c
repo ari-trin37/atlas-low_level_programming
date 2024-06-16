a71c4e463b4698ad2c7303831e13583e1eafae59
FQX117 <blackfox950@gmail.com>
2024-06-16 15:22:28 -0500

    fix betty

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -6,31 +6,34 @@
 int main(int argc, char *argv[])
 {
 	int num;
-    int sum;
-    int i;
+	int sum;
+	int i;
 
 	sum = 0;
 
-    if (argc == 1) 
+	if (argc == 1)
 	{
-        printf("0\n");
-        return (0);
-    }
+		printf("0\n");
+		return (0);
+	}
 
-    for (i = 1; i < argc; i++) {
-        int j = 0;
-        while (argv[i][j] != '\0') {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return (1);
-            }
-            j++;
-        }
+	for (i = 1; i < argc; i++)
+	{
+		int j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
 
-        num = atoi(argv[i]);
-        sum += num;
-    }
+		num = atoi(argv[i]);
+		sum += num;
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }
