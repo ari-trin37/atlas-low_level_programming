9244cd842898b8d2cd6265199f25085136471eb3
FQX117 <blackfox950@gmail.com>
2024-06-16 15:14:21 -0500

    fix

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -5,21 +5,21 @@
 
 int main(int argc, char *argv[])
 {
-    int num1;
-    int num2;
-    int result;
+	int num1;
+	int num2;
+	int result;
+	
+	if (argc != 3) {
+		printf("Error\n");
+		return 1;
+	}
 
-    if (argc != 3) {
-        printf("Error\n");
-        return 1;
-    }
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
+	result = num1 * num2;
 
-    result = num1 * num2;
+	printf("%d\n", result);
 
-    printf("%d\n", result);
-
-    return 0;
+	return (0);
 }
