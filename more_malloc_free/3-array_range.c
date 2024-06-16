439975970a982c5a008537cbd1bc260eeb049578
FQX117 <blackfox950@gmail.com>
2024-06-16 14:08:57 -0500

    test

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -3,7 +3,6 @@
  * malloc_checked -creating function that allocates memory
  * @min: variable #1 / the min amount of value.
  * @max: variable #2, the max amount of value.
- *@array_range: proto
  *
  * Return: returns pointer to the allocated memory.
  **/
