da0d1595e981340cb49b585ba17f7501fce4bb2a
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-15 22:16:05 -0500

    declared & moved things around

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,13 +12,18 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	int s1;
+	int s2;
+	unsigned int;
+	unsigned int = i
+
 	if (s1 == NULL) 
 		s1 = ""; /* empty string */
 	if (s2 == NULL)
 		s2 = ""; /* empty string */
 
-	unsigned int len_s1 = strlen(s1);
-	unsigned int len_s2 = strlen(s2);
+	i len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
 
 	unsigned int concat_len = len_s1 = ((n >= len_s2) ? len_s2 : n); /* determine actual length */
 
