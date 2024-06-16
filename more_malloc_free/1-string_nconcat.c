0c75a8fc6cf7a0769f762ebb016bc35fde3737f5
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-15 22:11:03 -0500

    commit 2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "string.h"
+#include <stdlib.h>
 /**
  * string_nconcat - function that links two strings.
  * @s1: 1st string 
@@ -11,7 +12,10 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	if (s1 == NULL) s1 = ""; /* treating NULL as empty string */
+	if (s1 == NULL) 
+		s1 = ""; /* empty string */
+	if (s2 == NULL)
+		s2 = ""; /* empty string */
 
 	unsigned int len_s1 = strlen(s1);
 	unsigned int len_s2 = strlen(s2);
@@ -19,18 +23,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int concat_len = len_s1 = ((n >= len_s2) ? len_s2 : n); /* determine actual length */
 
 	char *result = (char *)malloc(concat_len + 1); /* +1 for null-terminator */
-
 	if (result == NULL) /* mem. allocation failure */
-	{
-		return (NULL);
-	}
-
-	strcpy(result, s1); /* copies s1 into result */
-
-	strncat(result, s2, n); 
-
-	return (result); 
-
-
-
+		return(NULL);
 }
