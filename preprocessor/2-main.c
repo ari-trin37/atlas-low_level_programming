6d712addf1193b9fd9cc35337191dc3b1e862e66
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-17 20:28:34 -0500

    attempt to commit with GH - having difficulties with GH connec ting to VS code

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -3,11 +3,11 @@
 
 void print_2-main.c(void)
 {
-	char *2-main.c = __FILE__;
-	while (*2-main.c)
+	char *main.c = __main.c__;
+	while (*main.c)
 		{ 
-			_putchar(*2-main.c);
-			2-main.c++;
+			_putchar(*main.c);
+			main.c++;
 		}
 		_putchar('\n');
 		exit(0);
