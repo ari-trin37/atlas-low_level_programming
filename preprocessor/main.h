d57bf2cc58f1ce7949f0bd847a525310bb10e48f
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-17 21:00:32 -0500

    commit AGAIN

--- preprocessor/main.h
+++ preprocessor/main.h
@@ -5,10 +5,10 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-0-object_like_macro.h
-1-pi.h
-2-main.c
-3-function_like_macro.h
-4-sum.h
+0-object_like_macro.h;
+1-pi.h;
+2-main.c;
+3-function_like_macro.h;
+4-sum.h;
 
 #endif
