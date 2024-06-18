575f5d6c70df14ab0345ac2f58fb324f57790969
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-17 20:12:54 -0500

    test1

---
+++ preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "2-main.c"
+#include <stdlib.h>
+
+void print_2-main.c(void)
+{
+	char *2-main.c = __FILE__;
+	while (*2-main.c)
+		{ 
+			_putchar(*2-main.c);
+			2-main.c++;
+		}
+		_putchar('\n');
+		exit(0);
+}
