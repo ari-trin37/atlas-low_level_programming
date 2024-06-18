ddfef079f55e92f87e7a1271d1ebf6485c5dceca
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-18 16:32:39 -0500

    commit 1

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,7 +1,11 @@
 #ifndef function_like_macro.h
 #define function_like_macro.h
 
+/**
+ * ABS - determines absolute value of variable x.
+ * @x: main variable created.
+ */
 
-
+#define ABS(x) ((x) < ? -(X) : (x))
 
 #endif
