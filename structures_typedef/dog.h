7e252e7cd47055ce99e749792ed0eb19e1c4b081
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-20 16:05:51 -0500

    added to header file, 1st time

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef STRUCTURE_POINTERS_H
+#define STRUCTURE_POINTERS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/* create type def with structure /*
+typedef struct dog_s /* structure is dog_s*/
+{
+	char *name; 
+	float age; 
+	char *owner;
+} dog; 
+
+#endif
