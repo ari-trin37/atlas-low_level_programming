9445c6936c7a39b213740afaafcc04a7d0adcef0
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-20 17:01:37 -0500

    added prototypes to header file, etc.

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog.
+ * @d: 
+ * @name:
+ * @age:
+ * @owner:
+ *
+ * Return: doesn't return anything.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
