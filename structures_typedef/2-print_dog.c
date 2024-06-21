184e39d4162f504e9dbb450b4c6486e7cca6d694
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-21 13:54:22 -0500

    making betty happy

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 /**
- * print_dog - takes a pointer to a "struct dog" as argument, meaning no value returned.
+ * print_dog - takes pointer to "struct dog" as argument.
  * @d: variable // if 'd' is = to NULL, function prints nothing.
  *
  * Return: doesn't return anything due to void.
@@ -8,27 +8,25 @@
 
 void print_dog(struct dog *d)
 {
-    if (d == NULL) 
+	if (d == NULL)
 	{
-        return;
-    }
+		return;
+	}
 
-    printf("Name: ");
-    if (d->name == NULL) 
+	printf("Name: ");
+	if (d->name == NULL)
 	{
-        printf("(nil)\n");
-    } else {
-        printf("%s\n", d->name);
-    }
+		printf("(nil)\n");
+	} else {
+		printf("%s\n", d->name);
+	}
 
-    printf("Age: %.6f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 
-    printf("Owner: ");
-    if (d->owner == NULL) {
-        printf("(nil)\n");
-    } else {
-        printf("%s\n", d->owner);
-    }
+	printf("Owner: ");
+	if (d->owner == NULL) {
+		printf("(nil)\n");
+	} else {
+		printf("%s\n", d->owner);
+	}
 }
-
-
