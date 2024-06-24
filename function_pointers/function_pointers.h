0c6ad6770bd51aba4c346e50487167f1fd5faca8
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-24 16:51:59 -0500

    fix

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,14 +2,14 @@
 #define MY_HEADER_H
 #include <stdlib.h>
 #include <stdio.h>
-_putchar.c
+_putchar.c;
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-3-main.c
-3-op_functions.c
-3-get_op_func.c
-3-calc.h
+3-main.c;
+3-op_functions.c;
+3-get_op_func.c;
+3-calc.h;
 
 #endif
