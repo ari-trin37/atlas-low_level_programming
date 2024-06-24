5c73ff4c610e2d6cd29ed50f53a220efabdeed71
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-24 16:56:21 -0500

    test

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -7,9 +7,10 @@ _putchar.c;
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-3-main.c;
-3-op_functions.c;
-3-get_op_func.c;
-3-calc.h;
+
+#include "3-main.c"
+#include "3-op_functions.c"
+#include "3-get_op_func.c"
+#include "3-calc.h"
 
 #endif
