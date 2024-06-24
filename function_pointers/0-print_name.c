e76f6ade01aa6e96e68d81fcc1e1c2e795467bd3
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-24 17:48:50 -0500

    if this works:(

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -8,37 +8,9 @@
  * Return: return type is void.
  */
 
-char *copy;
 
-void _putchar(char c) {
-    write(1, &c, 1);
-}
-
-char *_strdup(const char *str) {
-    size_t len = 0;
-    const char *tmp = str;
-    while (*tmp++) {
-        len++;
-    }
-	*copy = (char *)malloc(len + 1);
-    if (copy == NULL) {
-        return NULL;
-    }
-    char *ptr = copy;
-    while (*str) {
-        *ptr++ = *str++;
-    }
-    *ptr = '\0';
-    return copy;
-}
 
 void print_name(char *name, void (*f)(char)) {
-    char *copy = _strdup(name);
-    if (copy == NULL) {
-        return;
-    }
-    while (*copy) {
-        f(*copy++);
-    }
-    free(copy);
+{
+	f(name);
 }
