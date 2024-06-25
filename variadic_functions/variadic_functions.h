28f73824b120a41f4a3219f403c4f49145903a5c
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-24 20:41:45 -0500

    all header files

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
