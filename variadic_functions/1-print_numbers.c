2807412dbf7bd4582f392d15a1f8843214937ac2
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-24 21:34:55 -0500

    attempt1. wish me luck.

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,8 +1,27 @@
 #include "variadic_functions.h"
-#include <stdarg.h>"
+#include "<stdarg.h>"
 /**
- * function - 
- * @:
- * @:
- * Return: 
+ * print_numbers - printing #'s w/ new line.
+ * @separator: string printed between #'s.
+ * @n: # of int's that'll be printed.
+ * @...: # of int's (variable)
+ * Return: printing set of #'s.
  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+	va_list args;
+    unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; 9 < n; i++)
+		{
+			printf("%d", va_arg(args, int));
+		}
+	if (separator != NULL && i < n - 1)
+        {
+			printf("%s", separator);
+		}
+	printf("/n");
+
+	va_end(args);
