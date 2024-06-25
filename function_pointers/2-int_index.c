231320d1e547472b19b8b2135e9a9529f1bc2784
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-24 20:25:00 -0500

    test

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,8 +1,26 @@
-#include "functions_pointers.h"
-#include <stdio>
+#include "function_pointers.h"
 /**
- * function - 
- * @:
- * @:
- * Return:
+ * int_index - if no element matches cmp func., return -1. otherwise, 0.
+ * @array: symbolizes first pointer of array.
+ * @size: # of elements in array
+ * @cmp: function that takes 'int' as para, returns int.
+ *
+ * Return: cmp doesn't return 0; if no element matches, return -1.
  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array == NULL || cmp == NULL || size <= 0)
+    	{
+			return(-1);
+    	}
+
+    for (int i = 0; i < size; i++) {
+        if (cmp(array[i]) != 0)
+		{
+            return(i);
+        }
+    }
+
+    return -1;
+}
