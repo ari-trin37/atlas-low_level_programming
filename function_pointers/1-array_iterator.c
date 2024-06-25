5764cadfd638b6505b95cfa85bd9e12128dd6c26
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-24 20:09:15 -0500

    testing 1 arrays

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,8 +1,18 @@
 #include "functions_pointers.h"
 #include <stdio>
 /**
- * function - 
- * @:
- * @:
- * Return:
+ * @array: a pointer, starts at array
+ * @size: size of array.
+ * @action: pointer to a function.
+ *
+ * Return: returns void.
  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+    for (i = 0; i < size; i++)
+    {
+		action(array[i]);
+    }
+}
