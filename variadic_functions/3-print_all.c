366aa631031262be57bfa39c43bc2650ff5faef8
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-26 12:51:15 -0500

    attempt

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,8 +1,58 @@
 #include "variadic_functions.h"
-#include <stdarg.h>"
+#include "<stdarg.h>"
 /**
- * function - 
- * @:
- * @:
- * Return: 
+ * print_all - 
+ * @char: character length
+ * @format: 
+ *
+ * Return:
  */
+
+va_list args;
+{
+    int i = 0;
+    char *str;
+    char c;
+    int num;
+    float fnum;
+
+    va_start(args, format);
+
+    while (format && format[i])
+	{
+        if (format[i] == 'c')
+		{
+            c = va_arg(args, int);
+            printf("%c", c);
+        } else if (format[i] == 'i')
+
+		{
+            num = va_arg(args, int);
+            printf("%d", num);
+        }
+
+		else if (format[i] == 'f')
+
+		{
+            fnum = va_arg(args, double);
+            printf("%f", fnum);
+        }
+
+		else if (format[i] == 's')
+		{
+            str = va_arg(args, char *);
+            if (str == NULL) {
+                printf("(nil)");
+            } else
+
+			{
+                printf("%s", str);
+            }
+        }
+        i++;
+    }
+
+    printf("\n");
+
+    va_end(args);
+}
