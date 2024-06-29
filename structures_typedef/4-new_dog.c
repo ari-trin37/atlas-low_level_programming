875335e472cb9f490900d416d327eeb3b69ad24c
ari-trin37 <arielle.taylor@atlasschool.com>
2024-06-28 19:37:04 -0500

    removed additional comments, fixed some lines

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
  */
 
 dog_t *new_dog(char *name, float age, char *owner) {
-
+{
 	dog_t *newdog;
 	char name;
 	float age;
@@ -21,49 +21,50 @@ dog_t *new_dog(char *name, float age, char *owner) {
     }
 
     newdog = (dog_t *)malloc(sizeof(dog_t));
-    if (newdog == NULL) {
+    if (newdog == NULL) 
+	{
         return NULL;
     }
 
-    // Calculate length of name
     int name_len = 0;
-    while (name[name_len] != '\0') {
+    while (name[name_len] != '\0')
+	{
         name_len++;
     }
 
-    // Allocate memory for newdog->name
     newdog->name = (char *)malloc(name_len + 1);
-    if (newdog->name == NULL) {
+    if (newdog->name == NULL)
+	{
         free(newdog);
         return NULL;
     }
 
-    // Copy name to newdog->name
-    for (int i = 0; i <= name_len; i++) {
+    for (int i = 0; i <= name_len; i++)
+	{
         newdog->name[i] = name[i];
     }
 
-    // Calculate length of owner
     int owner_len = 0;
-    while (owner[owner_len] != '\0') {
+    while (owner[owner_len] != '\0')
+	{
         owner_len++;
     }
 
-    // Allocate memory for newdog->owner
     newdog->owner = (char *)malloc(owner_len + 1);
-    if (newdog->owner == NULL) {
+	if (newdog->owner == NULL)
+	{
         free(newdog->name);
         free(newdog);
         return NULL;
     }
 
-    // Copy owner to newdog->owner
-    for (int i = 0; i <= owner_len; i++) {
+    for (int i = 0; i <= owner_len; i++)
+	{
         newdog->owner[i] = owner[i];
     }
 
     newdog->age = age;
 
-    return newdog;
+    return(newdog);
 }
 
